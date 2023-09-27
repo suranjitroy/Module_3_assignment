b24e6ff3ab957ceef7c68e8054d8e85b09a23092
suranjitroy <s.surunjit@gmail.com>
2023-09-27 16:08:44 +0600

    Module-3 Assignment

--- task_3.php
+++ task_3.php
@@ -1,13 +1,5 @@
 <?php
 
-//$grades = [85, 92, 78, 88, 95];
-
-// arsort($grades);
-
-// foreach($grades as $grade){
-//     echo $grade."<br/>";
-// }
-
 echo "<h1>Task 3: Array Sorting</h1> <br/>";
 
 function sorting($grades){
